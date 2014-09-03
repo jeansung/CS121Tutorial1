3964e889bc02712fb5261a4ea900cff55b147744
jeansung <jsung@hmc.edu>
2014-09-03 10:38:17 -0700

    Initial Commit

---
+++ BullsEye/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BullsEye
+//
+//  Created by Jean Sung on 9/3/14.
+//  Copyright (c) 2014 Jean Sung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
