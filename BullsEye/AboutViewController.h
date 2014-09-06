47b8c705fd6094d107edac664a9495fb1d4763ab
jeansung <jsung@hmc.edu>
2014-09-06 16:42:57 -0700

    added new about screen
    - added a new view controller for the information screen
    - added content to new screen
    -linked screen with storyboard and added close logic

---
+++ BullsEye/AboutViewController.h
@@ -0,0 +1,14 @@
+//
+//  AboutViewController.h
+//  BullsEye
+//
+//  Created by Jean Sung on 9/6/14.
+//  Copyright (c) 2014 Jean Sung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+-(IBAction)close;
+
+@end
